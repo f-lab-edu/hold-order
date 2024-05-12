88b03aef42033564d5fe3b0551361c5936712c1b
LeeJungMok <mearyne@gmail.com>
2024-05-12 22:24:52 +0900

    Project Start

---
+++ src/main/java/com/holdOrder/HoldOrder/HoldOrderApplication.java
@@ -0,0 +1,13 @@
+package com.holdOrder.HoldOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoldOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoldOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/holdOrder/HoldOrder/HoldOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holdOrder.HoldOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoldOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
