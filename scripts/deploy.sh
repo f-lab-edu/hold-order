24c2026b9259ad498caf19a3e584e4b35ba3837a
LeeJungMok <99131755+mearyne@users.noreply.github.com>
2024-05-15 00:38:25 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,30 +2,30 @@
 
 REPOSITORY=/home/ubuntu/app
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+echo "> 현재 구동 중인 애플리케이션 pid 확인" >> $REPOSITORY/log/deploy.log
 
 CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID" >> $REPOSITORY/log/deploy.log
 
 if [ -z "$CURRENT_PID" ]; then
-  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다." >> $REPOSITORY/log/deploy.log
 else
-  echo "> kill -15 $CURRENT_PID"
+  echo "> kill -15 $CURRENT_PID" >> $REPOSITORY/log/deploy.log
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 애플리케이션 배포" >> $REPOSITORY/log/deploy.log
 
 JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
 
-echo "> JAR NAME: $JAR_NAME"
+echo "> JAR NAME: $JAR_NAME" >> $REPOSITORY/log/deploy.log
 
-echo "> $JAR_NAME 에 실행권한 추가"
+echo "> $JAR_NAME 에 실행권한 추가" >> $REPOSITORY/log/deploy.log
 
 chmod +x $JAR_NAME
 
-echo "> $JAR_NAME 실행"
+echo "> $JAR_NAME 실행" >> $REPOSITORY/log/deploy.log
 
 nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
