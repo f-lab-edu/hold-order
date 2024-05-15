6e6d8c8db3ae3161a01335c65bab34ef680a7616
LeeJungMok <mearyne@gmail.com>
2024-05-15 09:57:35 +0900

    AWS에서 제대로 동작이 안되서 테스트 중

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,32 +1,31 @@
 #!/usr/bin/env bash
 
 REPOSITORY=/home/ubuntu/app
-JAR_NAME=$(pgrep -fla java | grep HoldOrder | awk '{print $5}')
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+
 CURRENT_PID=$(pgrep -fla java | grep HoldOrder | awk '{print $1}')
 
-mkdir /home/ubuntu/app/log
-
-echo "> 현재 구동 중인 애플리케이션 pid 확인" >> $REPOSITORY/log/deploy.log
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID" >> $REPOSITORY/log/deploy.log
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다." >> $REPOSITORY/log/deploy.log
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID" >> $REPOSITORY/log/deploy.log
+  echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> 새 애플리케이션 배포" >> $REPOSITORY/log/deploy.log
+echo "> 새 애플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
 
-echo "> JAR NAME: $JAR_NAME" >> $REPOSITORY/log/deploy.log
+echo "> JAR NAME: $JAR_NAME"
 
-echo "> $JAR_NAME 에 실행권한 추가" >> $REPOSITORY/log/deploy.log
+echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
-echo "> $JAR_NAME 실행" >> $REPOSITORY/log/deploy.log
+echo "> $JAR_NAME 실행"
 
 nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
