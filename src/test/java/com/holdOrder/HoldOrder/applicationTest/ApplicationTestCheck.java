2942914b929e7839258091a49afee042d0e9a9e4
LeeJungMok <mearyne@gmail.com>
2024-05-19 01:11:52 +0900

    application.yml 파일의 수정으로 인한 테스트 변화

--- src/test/java/com/holdOrder/HoldOrder/applicationTest/ApplicationTestCheck.java
+++ src/test/java/com/holdOrder/HoldOrder/applicationTest/ApplicationTestCheck.java
@@ -20,6 +20,6 @@ public class ApplicationTestCheck {
     @Test
     @DisplayName("테스트가 application-test 설정파일을 잘 준수하면서 실행되는지 확인")
     public void work_in_application_test_setting() {
-        assertThat(((HikariDataSource) dataSource).getJdbcUrl()).isEqualTo("jdbc:h2:mem:test;MODE=MySQL");
+        assertThat(((HikariDataSource) dataSource).getJdbcUrl()).startsWith("jdbc:h2:mem:test;MODE=MySQL");
     }
 }
