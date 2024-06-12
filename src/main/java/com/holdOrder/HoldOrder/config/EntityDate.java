bc0ec10438597616eced3b08427f64c96904b57d
LeeJungMok <mearyne@gmail.com>
2024-06-12 23:28:13 +0900

    createdUser, modifiedUser 추가함

--- src/main/java/com/holdOrder/HoldOrder/config/EntityDate.java
+++ src/main/java/com/holdOrder/HoldOrder/config/EntityDate.java
@@ -16,9 +16,15 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public abstract class EntityDate {
+    @Column(name = "createdUser")
+    private String createdUser;
+
     @CreatedDate
     @Column(nullable = false, updatable = false, name = "created_at")
     private LocalDateTime createdAt;
+
+    @Column(name = "modifiedUser")
+    private String modifiedUser;
 
     @LastModifiedDate
     @Column(name = "modified_at")
