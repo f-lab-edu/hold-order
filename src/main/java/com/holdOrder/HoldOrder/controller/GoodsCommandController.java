f96235dedeb2f3d9a0b5e55d0c29c75a92cee9e8
LeeJungMok <mearyne@gmail.com>
2024-06-27 23:43:22 +0900

    컨트롤러 경로를 적절히 변경함

--- src/main/java/com/holdOrder/HoldOrder/controller/GoodsCommandController.java
+++ src/main/java/com/holdOrder/HoldOrder/controller/GoodsCommandController.java
@@ -1,20 +1,16 @@
 package com.holdOrder.HoldOrder.controller;
 
 import com.holdOrder.HoldOrder.service.GoodsCommandService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/goods")
+@RequestMapping("/v1/goods")
+@RequiredArgsConstructor
 public class GoodsCommandController {
     private final GoodsCommandService goodsCommandService;
-
-    @Autowired
-    public GoodsCommandController(GoodsCommandService goodsCommandService) {
-        this.goodsCommandService = goodsCommandService;
-    }
 
 
 
